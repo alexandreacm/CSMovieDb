d3c82948096e1d6cb84034a19492ac425b1db032
Alexandre Marques <alexandreacm.marques@gmail.com>
2024-05-15 13:45:12 -0300

    feat: added google fonts
    Added @google-fonts/inter.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,45 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded && !fontError) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={styles.text1}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={styles.text2}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +51,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontFamily: "Inter_900Black",
+  },
+  text1: {
+    fontFamily: "Inter_400Regular",
+  },
+  text2: {
+    fontFamily: "Inter_300Light",
   },
 });
